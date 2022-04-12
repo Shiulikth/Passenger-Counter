d9a2e51e7cfce261ddba8fab221ef08479c746e6
ShiuliKth <shiulimondoll25@gmail.com>
2022-04-12 14:10:02 +0200

    calculation

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-//document.getElementById("count-el").innerText = 10;
-
-//initilize the count as 0
-//listen for clicks on the increment button
-//increment the count variable when button is clicked
-//change the count-el in the html to reflect the new count
+let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
 let count = 0;
 function increment(){
-    count = count +1;
-    countEl.innerText = count;
+    count +=1;
+    countEl.textContent = count;
+    
    
 }
 
 function save(){
-    console.log(count)
+    let value = count + " - ";
+    saveEl.textContent +=  value;
+    countEl.textContent = 0;
+    count = 0;
+   
+
 
 }
+
+
